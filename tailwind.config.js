2bfc91953e53a82c7a449a1343862206854f4fe3
Arslan Younis <arslancs15@gmail.com>
2025-04-15 10:35:55 +0500

    small fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
         background: "hsl(var(--background))",
         foreground: "hsl(var(--foreground))",
         primary: {
-          DEFAULT: "#000",
+          DEFAULT: "hsl(var(--primary))",
           foreground: "hsl(var(--primary-foreground))",
         },
         secondary: {
