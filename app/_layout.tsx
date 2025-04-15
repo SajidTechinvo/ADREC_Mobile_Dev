1d490107e8ca9352da0fa2f8dd246e054d0bb27c
Arslan Younis <arslancs15@gmail.com>
2025-04-15 10:01:26 +0500

    required changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,28 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
+import {
+  DarkTheme,
+  DefaultTheme,
+  ThemeProvider,
+  Theme,
+} from "@react-navigation/native";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { NAV_THEME } from "~/lib/constants";
+import { useColorScheme } from "~/lib/useColorScheme";
 import { useEffect } from "react";
 import "react-native-reanimated";
 import "../global.css";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
+const LIGHT_THEME: Theme = {
+  ...DefaultTheme,
+  colors: NAV_THEME.light,
+};
+const DARK_THEME: Theme = {
+  ...DarkTheme,
+  colors: NAV_THEME.dark,
+};
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
@@ -16,9 +30,9 @@ SplashScreen.preventAutoHideAsync();
 const queryClient = new QueryClient({});
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
+  const { isDarkColorScheme } = useColorScheme();
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
 
   useEffect(() => {
@@ -33,12 +47,13 @@ export default function RootLayout() {
 
   return (
     <QueryClientProvider client={queryClient}>
-      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        <Stack>
-          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-          <Stack.Screen name="+not-found" />
-        </Stack>
-        <StatusBar style="auto" />
+      <ThemeProvider value={isDarkColorScheme ? DARK_THEME : LIGHT_THEME}>
+        <StatusBar style={isDarkColorScheme ? "light" : "dark"} />
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        />
       </ThemeProvider>
     </QueryClientProvider>
   );
--- app/public/login/index.tsx
+++ app/public/login/index.tsx
@@ -1,10 +1,17 @@
-import { ScrollView, Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
 
 const Login = () => {
   return (
-    <ScrollView contentContainerStyle={{ flex: 1 }}>
+    <ScrollView
+      contentContainerStyle={{ flex: 1, marginTop: 100, paddingHorizontal: 16 }}
+    >
       <View style={{ flex: 1 }}>
-        <Text>Login</Text>
+        <Text className="text-right">Login</Text>
+        <Button variant="secondary">
+          <Text> Default</Text>
+        </Button>
       </View>
     </ScrollView>
   );
