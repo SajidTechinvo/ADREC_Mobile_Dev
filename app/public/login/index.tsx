ddbed3555c4802905c32c22b1b07c87db5171b10
Omyr-5 <umairmunawar536@gmail.com>
2025-04-15 17:07:07 +0500

    feat: implement login form with username and password fields, remember me checkbox, and SVG icons

--- app/public/login/index.tsx
+++ app/public/login/index.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { ScrollView, View } from "react-native";
+import { LockIconSvg } from "~/assets/icon/svg.assets";
 import { Button } from "~/components/ui/button";
+import { Checkbox } from "~/components/ui/checkbox";
+import { InputField } from "~/components/ui/inputField";
 import { Text } from "~/components/ui/text";
 
 const Login = () => {
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <ScrollView
       contentContainerStyle={{ flex: 1, marginTop: 100, paddingHorizontal: 16 }}
     >
-      <View style={{ flex: 1 }}>
+      <View style={{ flex: 1, gap: 10, padding: 20 }}>
         <Text className="text-right">Login</Text>
         <Button variant="secondary">
           <Text> Default</Text>
         </Button>
+        <InputField label="Username" required />
+        <InputField
+          placeholder="Type something..."
+          size="default"
+          label="Password"
+          isPassword
+          required
+          startIcon={<LockIconSvg />}
+        />
+        <Checkbox
+          checked={isChecked}
+          onToggle={() => setIsChecked(!isChecked)}
+          label="Remember me"
+        />
       </View>
     </ScrollView>
   );
---
+++ components/ui/inputField.tsx
@@ -0,0 +1,78 @@
+import { cva, VariantProps } from "class-variance-authority";
+import React, { useState } from "react";
+import { Pressable, Text, TextInput, TextInputProps, View } from "react-native";
+import { EyeCloseIconSvg, EyeOpenIconSvg } from "~/assets/icon/svg.assets";
+import { cn } from "~/lib/utils";
+
+const inputVariants = cva(
+  "flex-row justify-between items-center border border-[#dadadb] rounded-md  bg-input-bg-color  w-full",
+  {
+    variants: {
+      size: {
+        sm: "h-[30px]",
+        default: "h-[50px] bg-[#f0f3f5]",
+        lg: "h-[70px]",
+      },
+    },
+    defaultVariants: {
+      size: "default",
+    },
+  }
+);
+
+type InputProps = TextInputProps &
+  VariantProps<typeof inputVariants> & {
+    label?: string;
+    required?: boolean;
+    startIcon?: React.ReactNode;
+    endIcon?: React.ReactNode;
+    isPassword?: boolean;
+    containerClassName?: string;
+  };
+
+const InputField = ({
+  label,
+  required,
+  startIcon,
+  endIcon,
+  isPassword,
+  className,
+  containerClassName,
+  size,
+  ...props
+}: InputProps) => {
+  const [secure, setSecure] = useState(isPassword);
+
+  return (
+    <View className={cn("w-full space-y-1 gap-3", containerClassName)}>
+      {label && (
+        <Text className="text-xs text-text-dark-color font-bold">
+          {label} {required && <Text className="text-red-500 ">*</Text>}
+        </Text>
+      )}
+      <View className={cn(inputVariants({ size }), className)}>
+        <View className="flex-row items-center w-full px-3">
+          {startIcon && <View className="mr-2">{startIcon}</View>}
+          <TextInput
+            className=" text-base text-[placeholder-color]  w-full"
+            secureTextEntry={secure}
+            placeholderTextColor="#9CA3AF"
+            {...props}
+          />
+        </View>
+        {isPassword ? (
+          <Pressable
+            onPress={() => setSecure(!secure)}
+            className="absolute right-3"
+          >
+            {secure ? <EyeCloseIconSvg /> : <EyeOpenIconSvg />}
+          </Pressable>
+        ) : (
+          endIcon && <View className="ml-2">{endIcon}</View>
+        )}
+      </View>
+    </View>
+  );
+};
+
+export { InputField };
